f562ac6a5d64fbc23bcaddde43ebfa6fd888311f
Rina Pokale <rinapokale009@gmail.com>
2023-01-18 09:24:23 +0530

    program to count the digits of a given number using recursion

---
+++ src/main/java/recursion/CountDigitsOfNumUsingRecursion.java
@@ -0,0 +1,15 @@
+package recursion;
+
+//WAP to count the digits of a given number using recursion
+public class CountDigitsOfNumUsingRecursion {
+    public static void main(String[] args) {
+        System.out.println(countDigits(55));
+    }
+
+    private static int countDigits(int n){
+        if(n < 10)
+            return 1;
+
+        return 1 + countDigits(n/10);
+    }
+}
