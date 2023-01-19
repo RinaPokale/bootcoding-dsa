b502c386e95bd24b0e84f4a5b79f689fbf00f870
Rina Pokale <rinapokale009@gmail.com>
2023-01-19 17:55:23 +0530

    program to display x to the power n using recursion

---
+++ src/main/java/recursion/PowerOfNumUsingRecursion.java
@@ -0,0 +1,15 @@
+package recursion;
+
+public class PowerOfNumUsingRecursion {
+    public static void main(String[] args) {
+        System.out.println(powerOfNum(2, 0));
+        System.out.println(powerOfNum(0, 0));
+    }
+
+    private static int powerOfNum(int n, int pow){
+        if(pow == 0)
+            return 1;
+
+        return n * powerOfNum(n, pow-1);
+    }
+}
