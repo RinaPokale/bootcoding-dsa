84ea62f7409736f55cbad1cdf5741e897d4fbd33
Rina Pokale <rinapokale009@gmail.com>
2023-01-22 21:12:46 +0530

    Program to return second last element of linked list

---
+++ src/main/java/linkedList/ReturnSecondLastEleInLinList.java
@@ -0,0 +1,41 @@
+package linkedList;
+
+public class ReturnSecondLastEleInLinList {
+    private static Node createLinkedList(){
+        Node n1 = new Node(10);
+        Node n2 = new Node(30);
+        Node n3 = new Node(20);
+        Node n4 = new Node(30);
+        Node n5 = new Node(40);
+
+        Node head = n1;
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+
+        return head;
+    }
+
+    public static Node returnSecondLast(Node head){
+        Node temp = head;
+        Node prev = head;
+
+        while (temp!=null){
+            if(prev.next.next == null)
+                return prev;
+            else
+                prev = temp;
+
+            temp = temp.next;
+        }
+        return prev;
+
+    }
+
+    public static void main(String[] args) {
+        Node head = createLinkedList();
+        Node secondLast = returnSecondLast(head);
+        System.out.println(secondLast.data);
+    }
+}
