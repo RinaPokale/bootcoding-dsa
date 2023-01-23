e832ee077f1c23cb91a8378481c2ba842bf7f9e7
Rina Pokale <rinapokale009@gmail.com>
2023-01-23 10:47:02 +0530

    Program to detect cycle in linked list

--- src/main/java/linkedList/DetectCycleInLinList.java
+++ src/main/java/linkedList/DetectCycleInLinList.java
@@ -10,13 +10,13 @@ public class DetectCycleInLinList {
         Node n1 = new Node(1);
         Node n2 = new Node(2);
         Node n3 = new Node(0);
-        Node n4 = new Node(-4);
+//        Node n4 = new Node(-4);
 
         Node head = n1;
         n1.next = n2;
-        n2.next = n1;
-        n3.next = n4;
-        n4.next = n2;
+        n2.next = n3;
+        n3.next = n2;
+//        n4.next = n2;
 
         return head;
     }
