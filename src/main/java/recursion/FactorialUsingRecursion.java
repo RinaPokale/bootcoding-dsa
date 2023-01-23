ad9a8fcd865871b5e0920483fa4b9daa33ca0615
Rina Pokale <rinapokale009@gmail.com>
2023-01-23 23:27:05 +0530

    program to find the factorial of a given number using recursion

--- src/main/java/recursion/FactorialUsingRecursion.java
+++ src/main/java/recursion/FactorialUsingRecursion.java
@@ -2,7 +2,7 @@ package recursion;
 
 public class FactorialUsingRecursion {
     public static void main(String[] args) {
-        System.out.println(factorialOfNum(3));
+        System.out.println(factorialOfNum(6));
     }
 
     private static int factorialOfNum(int n){
