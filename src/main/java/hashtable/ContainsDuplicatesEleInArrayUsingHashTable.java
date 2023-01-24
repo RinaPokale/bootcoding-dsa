75f9d799d946e3d048950929b2caf5408b735a3f
Rina Pokale <rinapokale009@gmail.com>
2023-01-24 22:26:42 +0530

    program to check if array contains duplicate elements using hash table

---
+++ src/main/java/hashtable/ContainsDuplicatesEleInArrayUsingHashTable.java
@@ -0,0 +1,22 @@
+package hashtable;
+
+import java.util.HashMap;
+
+public class ContainsDuplicatesEleInArrayUsingHashTable {
+    public static boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> store = new HashMap <>();
+
+        for(int i = 0; i < nums.length; i++) {
+            if (store.containsKey(nums[i])){
+                return true;
+            }
+            store.put(nums[i], 1);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(containsDuplicate(nums));
+    }
+}
