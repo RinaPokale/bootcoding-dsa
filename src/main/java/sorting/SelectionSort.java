b1efb5adf153c173027aa21b9a3ed90a6ccee617
Rina Pokale <rinapokale009@gmail.com>
2023-01-27 09:44:06 +0530

    program to sort array using selection sort

---
+++ src/main/java/sorting/SelectionSort.java
@@ -0,0 +1,30 @@
+package sorting;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] num = {54, 2, 76, 8, 90, 1};
+        selectionSort(num);
+    }
+
+    public static void selectionSort(int[] num){
+        for(int i = 0; i < num.length-1; i++){
+            int min = i;
+            for(int j = i+1; j < num.length; j++){
+                if(num[j] < num[min])
+                    min = j;
+            }
+
+            //swapping
+            if(min != i){
+                int temp = num[min];
+                num[min] = num[i];
+                num[i] = temp;
+            }
+
+        }
+
+        for(int i = 0; i < num.length; i++){
+            System.out.print(num[i] + " ");
+        }
+    }
+}
