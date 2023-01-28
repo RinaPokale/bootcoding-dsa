48d94c46416779cb6060fbf1fa4eb7759bf98168
Rina Pokale <rinapokale009@gmail.com>
2023-01-28 21:34:58 +0530

    Climbing Stairs using memoization

---
+++ src/main/java/memoization/ClimbingStairs.java
@@ -0,0 +1,33 @@
+package memoization;
+
+import java.util.HashMap;
+
+public class ClimbingStairs {
+
+    static HashMap<Integer, Integer> store = new HashMap<>();
+
+    public static int climbStairs(int n) {
+        if(n == 1 || n == 2)
+            return n;
+
+        if(store.containsKey(n))
+            return store.get(n);
+
+        store.put(n, climbStairs(n-1) + climbStairs(n-2));
+        return store.get(n);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(5));
+    }
+}
+
+/*
+  if (store.containsKey(n)) {
+            return store.get(n);
+        }
+
+        store.put(n, climbStairs(n-1) + climbStairs(n-2));
+        return store.get(n);
+ */
