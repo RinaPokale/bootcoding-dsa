2f34ed57ae27ef394710c22d057558986d73d4a0
Rina Pokale <rinapokale009@gmail.com>
2023-01-29 18:57:29 +0530

    transpose of a matrix

---
+++ src/main/java/array/TransposeOfMatrix.java
@@ -0,0 +1,29 @@
+package array;
+
+public class TransposeOfMatrix {
+    public static void transposeOfMatrix(int[][] num){
+        int row = num.length;
+        int col = num[0].length;
+
+        int[][] transpose = new int[col][row];
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                transpose[j][i] = num[i][j];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+//        int[][] num = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] num = {{1, 2, 3}, {4, 5, 6}};
+        transposeOfMatrix(num);
+
+//        for(int i = 0; i < num[0].length; i++){
+//            for(int j = 0; j < num.length; j++){
+//                System.out.print(num[i][j]);
+//            }
+//            System.out.println();
+//        }
+    }
+}
