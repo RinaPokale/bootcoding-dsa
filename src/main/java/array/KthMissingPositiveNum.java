16f804419640c07435c0a46f607d2a7dc85f20ea
Rina Pokale <rinapokale009@gmail.com>
2023-01-29 19:50:34 +0530

    program to return Kth missing positive number

---
+++ src/main/java/array/KthMissingPositiveNum.java
@@ -0,0 +1,49 @@
+package array;
+
+public class KthMissingPositiveNum {
+
+    public static int returnKthMissingPositiveNumber(int[] num, int k){
+        int indx = 0;
+
+        for(int i = 1;; i++){
+            if(indx < num.length && i == num[indx]) {
+                indx++;
+                continue;
+            }
+            k--;
+            if(k == 0)
+                return i;
+        }
+    }
+
+    public static void main(String[] args) {
+//        int[] num = {2,3,4,7,11}; // k = 5
+        int[] num = {1,2,3,4};
+        int k = 2; // position -- > indx-1
+        System.out.println(returnKthMissingPositiveNumber(num, k));
+    }
+}
+
+
+/*
+ if(i != num[indx]) {
+                //if(!store.containsKey(num[i]))
+                store.put(i, count++);
+            }
+            else {
+                indx++;
+            }
+ */
+
+
+/*
+for(int i = 1; i <= 1000; i++){
+            if(indx < num.length && i == num[indx]) {
+                indx++;
+            }
+            else {
+                store.put(i, count++);
+            }
+        }
+
+ */
