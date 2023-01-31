ec7b50e96a306b34827928e753a0dc6dc332ccad
Rina Pokale <rinapokale009@gmail.com>
2023-01-31 20:06:51 +0530

    program to Find Greatest Common Divisor of Array

--- src/main/java/array/GCDOfArray.java
+++ src/main/java/array/GCDOfArray.java
@@ -2,6 +2,9 @@ package array;
 
 //1979. Find Greatest Common Divisor of Array
 
+/* Given an integer array nums, return the greatest common divisor of the smallest number and largest number in nums.
+The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers. */
+
 public class GCDOfArray {
     public static void main(String[] args) {
         int[] nums = {10,6, 9};
