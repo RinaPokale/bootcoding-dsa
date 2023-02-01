07accb0b768b4bc9401c4c1dfc4191a6f2906ea3
Rina Pokale <rinapokale009@gmail.com>
2023-02-01 23:09:39 +0530

    program to Increment the large integer by one and return the resulting array of digits.

--- src/main/java/array/PlusOne.java
+++ src/main/java/array/PlusOne.java
@@ -2,7 +2,7 @@ package array;
 
 //66. Plus One
 
-/*You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit
+/* You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit
 of the integer. The digits are ordered from most significant to the least significant in left-to-right order.
 The large integer does not contain any leading 0's. */
 
