f9be076a5f778dbd7383224811e5e3475b0db9ee
Rina Pokale <rinapokale009@gmail.com>
2023-02-01 23:05:39 +0530

    program to Increment the large integer by one and return the resulting array of digits.

---
+++ src/main/java/array/PlusOne.java
@@ -0,0 +1,28 @@
+package array;
+
+//Increment the large integer by one and return the resulting array of digits.
+public class PlusOne {
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
+    public static void main(String[] args) {
+//       int digits[]  = {9, 9, 9};
+       int[] digits = {3, 8, 9};
+       int[] sum = plusOne(digits);
+
+       for(int i = 0; i < sum.length; i++){
+           System.out.print(sum[i] + " ");
+       }
+    }
+}
