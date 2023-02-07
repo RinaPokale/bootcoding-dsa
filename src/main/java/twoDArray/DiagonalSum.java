0c4366aa88170f2e1e8b59fdf5478e558609895d
Rina Pokale <rinapokale009@gmail.com>
2023-02-07 10:28:20 +0530

    Matrix Diagonal Sum

---
+++ src/main/java/twoDArray/DiagonalSum.java
@@ -0,0 +1,19 @@
+package twoDArray;
+
+public class DiagonalSum {
+    public static int diagonalSum(int[][] matrix){
+        int sum = 0, n = matrix.length;
+        for(int i = 0; i < n; i++){ // O(n)
+            sum+= matrix[i][i]; // primary diagonal sum
+            if(i!=n-i-1)
+                sum+= matrix[i][n-i-1]; // secondary diagonal sum
+        }
+        return sum; // primary diagonal + secondary diagonal
+    }
+
+    public static void main(String[] args) {
+//        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8} , {9, 10, 11, 12}, {13, 14, 15, 16}};
+        System.out.println(diagonalSum(matrix));
+    }
+}
