e8a6452810326639b024af064dc731b7ac45936d
Rina Pokale <rinapokale009@gmail.com>
2023-02-08 22:06:51 +0530

    program to return the number of good pairs.

--- src/main/java/array/NumberOfGoodPairsInArray.java
+++ src/main/java/array/NumberOfGoodPairsInArray.java
@@ -2,8 +2,8 @@ package array;
 
 //1512. Number of Good Pairs
 
-//Given an array of integers nums, return the number of good pairs.
-//A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+/* Given an array of integers nums, return the number of good pairs.
+A pair (i, j) is called good if nums[i] == nums[j] and i < j. */
 
 public class NumberOfGoodPairsInArray {
     public static int returnCountOfNumberOfGoodPairs(int[] num){
