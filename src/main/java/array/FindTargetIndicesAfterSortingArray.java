b8b9def76cf597dadd3af22342edb4fc8a777103
Rina Pokale <rinapokale009@gmail.com>
2023-02-10 12:33:03 +0530

    program to Find Target Indices After Sorting Array

--- src/main/java/array/FindTargetIndicesAfterSortingArray.java
+++ src/main/java/array/FindTargetIndicesAfterSortingArray.java
@@ -7,6 +7,7 @@ import java.util.List;
 //2089. Find Target Indices After Sorting Array
 
 //You are given a 0-indexed integer array nums and a target element target.
+//A target index is an index i such that nums[i] == target.
 public class FindTargetIndicesAfterSortingArray {
     static List<Integer> list = new ArrayList<>();
 
