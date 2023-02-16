46ec4bf9c7871f27d0410b3f41bea1f5a42d3231
Rina Pokale <rinapokale009@gmail.com>
2023-02-16 21:42:31 +0530

    program to return indices of the two numbers such that they add up to target.

---
+++ src/main/java/hashtable/TwoSum.java
@@ -0,0 +1,32 @@
+package hashtable;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    static HashMap<Integer,Integer> store = new HashMap<>();
+    public static int[] twoSum(int[] nums, int target) {
+        int len = nums.length;
+        int[] ans = new int[2];
+
+        for(int i=0;i<len;i++){
+            if(store.containsKey(target-nums[i])){
+                ans[1]=i;
+                ans[0]=store.get(target-nums[i]);
+                return ans;
+            }
+            store.put(nums[i],i);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+//        int[] num = {3, 2, 4};
+        int[] num = {2, 7, 11, 15};
+        int target = 9;
+        int[] sum = twoSum(num, target);
+
+        for(int n: sum){
+            System.out.println(n);
+        }
+    }
+}
