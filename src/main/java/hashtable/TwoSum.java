b1e3f3b377b01486e383e10ccd5b76b2ae7e2715
Rina Pokale <rinapokale009@gmail.com>
2023-02-16 21:45:27 +0530

    program to return indices of the two numbers such that they add up to target.

--- src/main/java/hashtable/TwoSum.java
+++ src/main/java/hashtable/TwoSum.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 //1. Two Sum
 //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up
 // to target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
 public class TwoSum {
     static HashMap<Integer,Integer> store = new HashMap<>();
     public static int[] twoSum(int[] nums, int target) {
