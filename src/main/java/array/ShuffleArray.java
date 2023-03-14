2b075b3f43866c5256b909667ee6658e40c158ed
Rina Pokale <rinapokale009@gmail.com>
2023-03-14 20:39:58 +0530

    shuffle the array

--- src/main/java/array/ShuffleArray.java
+++ src/main/java/array/ShuffleArray.java
@@ -3,6 +3,7 @@ package array;
 //1470. Shuffle the Array
 
 //Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+//Return the array in the form [x1,y1,x2,y2,...,xn,yn].
 public class ShuffleArray {
     public static int[] shuffle(int[] nums, int n) {
         int[] suffleNum = new int[nums.length];
