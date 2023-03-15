74b88637145ca20d18dba154a6a1c28606bc0eb0
Rina Pokale <rinapokale009@gmail.com>
2023-03-15 20:20:36 +0530

    Write an algorithm to determine if a number n is happy.

--- src/main/java/array/HappyNumber.java
+++ src/main/java/array/HappyNumber.java
@@ -3,12 +3,13 @@ package array;
 import java.util.HashSet;
 
 //202. Happy Number
+
+//Return true if n is a happy number, and false if not.
 //A happy number is a number defined by the following process:
 //    Starting with any positive integer, replace the number by the sum of the squares of its digits.
 //    Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle
 //    which does not include 1.
 //    Those numbers for which this process ends in 1 are happy.
-//Return true if n is a happy number, and false if not.
 public class HappyNumber {
     //Approach-1
     public static boolean isHappy(int n) {
