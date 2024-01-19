fd85bc075f0cb2b8c38a5f16a4af75ab553fd3ec
Rina Pokale <rinapokale009@gmail.com>
2024-01-19 22:41:12 +0530

    First Unique Character in a String  - LeetCode Problem

--- src/main/java/hashing/hashTable/hashMap/FirstUniqueCharacterInString.java
+++ src/main/java/hashing/hashTable/hashMap/FirstUniqueCharacterInString.java
@@ -6,6 +6,8 @@ import java.util.concurrent.ConcurrentHashMap;
 
 //387. First Unique Character in a String
 
+//similar problem --> 451. Sort Characters By Frequency
+
 public class FirstUniqueCharacterInString {
     public static int firstUniqueChar(String s) {
 //        To avoid ConcurrentModificationException exception using ConcurrentHashMap
@@ -42,10 +44,56 @@ public class FirstUniqueCharacterInString {
         return -1;
     }
 
+
+    public static int firstUniqueCharTwo(String s){
+        Map<String, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+            String ch = String.valueOf(s.charAt(i));
+            if(!map.containsKey(ch)){
+                map.put(ch, 1);
+                continue;
+            }
+            map.put(ch, map.get(ch)+1);
+        }
+
+        //unique ele
+        for(int i = 0; i < s.length(); i++){
+            if (map.get(String.valueOf(s.charAt(i))) == 1)
+                return i;
+        }
+        return -1;
+    }
+
+
+    public static int firstUniqueCharThree(String s){
+        int[] chars = new int[26];
+        for(int i=0; i<s.length(); i++){
+            chars[s.charAt(i)-'a']++;
+        }
+
+        for(int i=0;i<s.length();i++){
+            if(chars[s.charAt(i)-'a'] == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         System.out.println(firstUniqueChar("leetcode"));
         System.out.println(firstUniqueChar("loveleetcode"));
         System.out.println(firstUniqueChar("aabb"));
+
+        System.out.println(firstUniqueCharTwo("leetcode"));
+        System.out.println(firstUniqueCharTwo("loveleetcode"));
+        System.out.println(firstUniqueCharTwo("aabb"));
+
+        System.out.println(firstUniqueCharThree("leetcode"));
+        System.out.println(firstUniqueCharThree("loveleetcode"));
+        System.out.println(firstUniqueCharThree("aabb"));
+
+
 
     }
 }
