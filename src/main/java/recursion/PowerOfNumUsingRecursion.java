d49836c4d943903c9e670dce451d3bdabf5291f5
Rina Pokale <rinapokale009@gmail.com>
2024-01-23 23:08:15 +0530

    Power Of Num Using Recursion

--- src/main/java/recursion/PowerOfNumUsingRecursion.java
+++ src/main/java/recursion/PowerOfNumUsingRecursion.java
@@ -3,6 +3,7 @@ package recursion;
 public class PowerOfNumUsingRecursion {
     public static void main(String[] args) {
         System.out.println(powerOfNum(2, 0));
+        System.out.println(powerOfNum(2, 2));
         System.out.println(powerOfNum(0, 0));
     }
 
