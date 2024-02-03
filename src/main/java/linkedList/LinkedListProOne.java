97d15e4c91953d75aea069d6bcf54977e03a2f34
Rina Pokale <rinapokale009@gmail.com>
2024-02-03 22:26:02 +0530

    construct Linked List

---
+++ src/main/java/linkedList/LinkedListProOne.java
@@ -0,0 +1,36 @@
+package linkedList;
+
+public class LinkedListProOne {
+    public NodeWithoutConstructor createLinkedList(){
+        NodeWithoutConstructor n1 = new NodeWithoutConstructor();
+        NodeWithoutConstructor n2 = new NodeWithoutConstructor();
+        NodeWithoutConstructor n3 = new NodeWithoutConstructor();
+
+        n1.data = 10; // n1 --> | 10 | ref |
+        n2.data = 20; // n2 --> | 20 | ref |
+        n3.data = 30; // n3 --> | 30 | ref |
+
+        NodeWithoutConstructor head = n1;
+        n1.next = n2;
+        n2.next = n3;
+
+        return head;
+    }
+
+    public static void printLinkedList(NodeWithoutConstructor head){
+        NodeWithoutConstructor temp = head;
+        while (temp!=null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedListProOne linkedListPro = new LinkedListProOne();
+
+        NodeWithoutConstructor head = linkedListPro.createLinkedList();
+        printLinkedList(head);
+    }
+
+
+}
