3c1c5651574b7c885992864e4ed1ff32be7f270d
Rina Pokale <rinapokale009@gmail.com>
2024-02-08 20:41:58 +0530

    creation Node

---
+++ src/main/java/linkedList/Node.java
@@ -0,0 +1,11 @@
+package linkedList;
+
+public class Node {
+    int data;
+    Node next; // reference or address
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
