d1ac3f45741e3d689a0c906d23edd4109a9117ee
Rina Pokale <rinapokale009@gmail.com>
2024-02-08 20:41:11 +0530

    Node Without Constructor

---
+++ src/main/java/linkedList/NodeWithoutConstructor.java
@@ -0,0 +1,11 @@
+package linkedList;
+
+public class NodeWithoutConstructor {
+    int data;
+    NodeWithoutConstructor next;
+
+    void node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
