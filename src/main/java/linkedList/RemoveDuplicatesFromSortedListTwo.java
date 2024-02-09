ae7f1111fbe6347ba80de1965e62b00f07bb40c4
Rina Pokale <rinapokale009@gmail.com>
2024-02-09 20:56:15 +0530

    Remove Duplicates from Sorted List II - LeetCode Problem

--- src/main/java/linkedList/RemoveDuplicatesFromSortedListTwo.java
+++ src/main/java/linkedList/RemoveDuplicatesFromSortedListTwo.java
@@ -4,8 +4,6 @@ package linkedList;
 /*Given the head of a sorted linked list, delete all nodes that have duplicate numbers,
 leaving only distinct numbers from the original list. Return the linked list sorted as well. */
 
-import java.util.HashMap;
-
 public class RemoveDuplicatesFromSortedListTwo {
     static class ListNode {
         int val;
